7e17f5c4df110d31172997da0657a5661599bc90
Sandro Vendeth <sandrohenrique92@outlook.com>
2023-04-28 10:19:26 -0300

    App finalizado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,30 @@
+import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, Alert, FlatList } from 'react-native';  //text input para colocar o input, TouchableOpacity para fazer clicks
 import { styles } from './styles';
 
 import { Participant } from '../../components/Participant';   // importando o componente Participant
 
-export default function Home() {  // sempre criar as funções dentro do escopo dos components (HOME)
-    const participants = ['André', 'Sandro', 'Gabriel', 'Felipe', 'Caio', 'João','Walquiria' ,'Rafael' ,'Joaquim']
+export default function Home() { // sempre criar as funções dentro do escopo dos components (HOME)
+    
+  const [participants, setParticipants] = useState<string[]>([]);  // [estado, função que vai atualizar o conteúdo do estado] ->string[] para determinar o que o conteúdo do array vazio é uma string
+  const [participantName, setParticipantName] = useState(''); // estado do nome do participante para pegar o valor digital pelo usuário 
 
     function handleParticipantAdd(){
-        if(participants.includes("André")){   //Verifica dentro do Array sem temos valores repetidos
+        if(participants.includes(participantName)){   //Verifica dentro do Array sem temos valores repetidos
            return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.") // Alerta
         }
+
+        setParticipants(prevState => [...prevState, participantName]) // função que define o novo conteúdo de participants // [...prevState para manter o que tinha antes e somar com o que queremos adicionar]
+        
+        setParticipantName(''); // limpar o estado ao adicionar o participante (input)
+    
     }
-    function handleParticipantRemove(name:string){ 
+
+    function handleParticipantRemove(name:string){   
         Alert.alert("Remover", `Remover o participante ${name}?`, [ // Alerta de confirmação
             {
                text: 'Sim',
-               onPress: () => Alert.alert("Deletado!")
+               onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name)) // LÓGICA: Aqui ele está acessando o conteúdo atual da lista e percorrendo esse estado através do filtro, para retornar todos os participantes menos o nome que será deletado
             },
             {
                 text: 'Não',
@@ -39,6 +48,8 @@ export default function Home() {  // sempre criar as funções dentro do escopo 
                 style={styles.input}
                 placeholder='Nome do Participante'
                 placeholderTextColor="#6B6b6b"
+                onChangeText={setParticipantName}  // disparada um evento sempre que o conteúdo do textinput muda, acessa o conteúdo atual da caixa de texto
+                value={participantName}
             />
             <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}/* propriedade on press no qual chama uma função ao pressionar o botão + */> 
                 <Text style={styles.buttonText}>
@@ -54,8 +65,10 @@ export default function Home() {  // sempre criar as funções dentro do escopo 
                 <Participant
                   key={item}
                   name={item}
-                  onRemove={() => handleParticipantRemove(item)} /* Passar a propriedade para o componente, neste caso como texto name e também podemos através do onRemove passar uma função como propriedade que exclui o participante*//>
-            )}
+                  onRemove={() => handleParticipantRemove(item)} /* Passar a propriedade para o componente, neste caso como texto name e também podemos através do onRemove passar uma função como propriedade que exclui o participante*/
+                  />
+                )}
+
             showsVerticalScrollIndicator={false}
         />
     </View>
